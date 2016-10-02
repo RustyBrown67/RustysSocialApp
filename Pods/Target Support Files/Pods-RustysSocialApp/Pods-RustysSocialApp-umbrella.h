c90f1be36c01a11e9a7a2bea0d886babf5f2593c
RustyBrown67 <apple@ians-mbp-2.home>
2016-10-02 20:34:27 +0100

    Basic UI Layout Completed

---
+++ Pods/Target Support Files/Pods-RustysSocialApp/Pods-RustysSocialApp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RustysSocialAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RustysSocialAppVersionString[];
+
---
+++ RustysSocialApp/SignInVC.swift
@@ -0,0 +1,24 @@
+//
+//  SignInVC.swift
+//  RustysSocialApp
+//
+//  Created by Russell Brown on 02/10/2016.
+//  Copyright © 2016 Russell Brown. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+
+
+}
+
